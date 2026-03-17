9dd1a1fbff80a0bd25fa660bd0a6f2bc85581bb8
bpmryan <bpm_ryand2428@gmail.com>
2026-03-16 20:49:57 -0500

    readjust test and implementation for gameboard

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,34 +1,34 @@
 class Gameboard {
-    constructor() {
-        this.board = Array.from({ length: 100 }, () => ({
-            hasShip: null, isShot: false
-        }));
-        this.ships = [];
-    }
+  constructor() {
+    this.board = Array.from({ length: 100 }, () => ({
+      hasShip: null, isShot: false
+    }));
+    this.ships = [];
+  }
 
-    placeShip(ship) {
-        ship.position.forEach(index => {
-            this.board[index].hasShip = ship;
-        });
-        this.ship.push(ship);
-    }
+  placeShip(ship) {
+    ship.position.forEach(index => {
+      this.board[index].hasShip = ship;
+    });
+    this.ships.push(ship);
+  }
 
-    receiveShot(index) {
-        const cell = this.board[index];
-        cell.isShot = true;
-        if (cell.hasShip) cell.hasShip.hit(index);
-    }
+  receiveShot(index) {
+    const cell = this.board[index];
+    cell.isShot = true;
+    if (cell.hasShip) cell.hasShip.hit(index);
+  }
 
-    opponentBoard() {
-        return this.board.map(cell => {
-            if (cell.isShot && cell.hasShip) return 'hit';
-            if (cell.isShot) return 'miss';
-            return 'empty';
-        });
-    }
+  opponentBoard() {
+    return this.board.map(cell => {
+      if (cell.isShot && cell.hasShip) return 'hit';
+      if (cell.isShot) return 'miss';
+      return 'empty';
+    });
+  }
 
-    allSunk() {
-        return this.ships.every(ship => ship.isSunk());
-    }
+  allSunk() {
+    return this.ships.every(ship => ship.isSunk());
+  }
 }
-module.exposts = Gameboard;
+module.exports = Gameboard;
